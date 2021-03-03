3fcba6e5d6227f07dc9e0eb1cb438c8ccb28adce
Annii <anish.1903.kmr@gmail.com>
2021-03-03 16:34:50 +0530

    Completed Day-5 Question

--- Day-5__MoveAllNegativeNumbersToOneSideOfArray.cpp
+++ Day-5__MoveAllNegativeNumbersToOneSideOfArray.cpp
@@ -1,21 +1,49 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-void sorter(int arr[], int n){
+void sorter(int arr[], int n)
+{
+
+    // Rearranging the array.
     int temp;
-    int start=0;
     int end = n;
-    for(int i=0; i<n; i++){
-        if(arr[i]<0){
-            arr[start]=arr[i];
-            start++;
-        }
-        else{
-            arr[end]=arr[i]
+    for (int i = 0; i < end; i++)
+    {
+        if (arr[i] > 0)
+        {
+            temp = arr[i];
+            for (int j = i; j < end; j++)
+            {
+                arr[j] = arr[j + 1];
+            }
+            arr[end - 1] = temp;
+            i--;
+            end--;
         }
     }
+
 }
 
-int main() {
-	//code
-	return 0;
+int main()
+{
+    //code
+    int N;
+    cin >> N;
+
+    int arr[N];
+    for (int k = 0; k < N; k++)
+    {
+        cin >> arr[k];
+    }
+
+    sorter(arr, N);
+
+    // Printing the rearranged array.
+    for (int l = 0; l < N; l++)
+    {
+        cout << arr[l];
+        cout << ",";
+    }
+
+    return 0;
 }
