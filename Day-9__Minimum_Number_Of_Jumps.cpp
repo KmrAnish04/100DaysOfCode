85e6e5c37ca31cfa6d74e8cf335a6acdc84a2b52
Annii <anish.1903.kmr@gmail.com>
2021-03-25 23:41:02 +0530

    Added Day-9

---
+++ Day-9__Minimum_Number_Of_Jumps.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int minJumps(int arr[], int n){
+        // Your code here
+        int jumps = 0;
+        int indexer1 = arr[0];
+        int indexer2 = 0;
+        jumps++;
+        
+        for(int i=indexer1; i<n; i++){
+            i--;
+            indexer2 = arr[i];
+            
+            if(indexer2 >= n-1-indexer1){
+                jumps++;
+                cout<<"in if"<<indexer1<<indexer2<<"\n";
+                break;
+            }
+            else{
+                indexer1 = indexer1 + indexer2;
+                i = indexer1;
+                cout<<"in else"<<indexer1<<indexer2<<"\n";
+                jumps++;
+            }
+        }
+        
+        cout<<jumps;
+        return jumps;
+    }
+
+int main(){
+    int n = 11;
+    int arr[11] = {1,3,5,8,9,2,6,7,6,8,9};
+
+    minJumps(arr, n);
+}
