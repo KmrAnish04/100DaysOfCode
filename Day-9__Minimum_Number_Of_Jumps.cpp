1d3cdd9659a01c3c2315d1a0e138465e5a027c7c
Annii <anish.1903.kmr@gmail.com>
2021-07-08 21:47:01 +0530

    Added some problelms

--- Day-9__Minimum_Number_Of_Jumps.cpp
+++ Day-9__Minimum_Number_Of_Jumps.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int minJumps(int arr[], int n){
         // Your code here
+        if (n <= 1)
+        return 0;
+        
+        if (arr[0] == 0)
+        return -1;
+
+
         int jumps = 0;
         int indexer1 = arr[0];
         int indexer2 = 0;
         jumps++;
-
-	jump++;
-    if(i>=maxReach)
-       return -1;
-    step = maxReach - i;
-	}
         
-        for(int i=indexer1; i<n; i++){
-            i--;
+        for(int i=indexer1; i<n;){
             indexer2 = arr[i];
             
             if(indexer2 >= n-1-indexer1){
@@ -36,8 +36,10 @@ int minJumps(int arr[], int n){
     }
 
 int main(){
-    int n = 11;
-    int arr[11] = {1,3,5,8,9,2,6,7,6,8,9};
+    // int n = 11;
+    // int arr[11] = {1,3,5,8,9,2,6,7,6,8,9};
+    int n =6;
+    int arr[6] = {1,4,3,2,6,7};
 
     minJumps(arr, n);
 }
