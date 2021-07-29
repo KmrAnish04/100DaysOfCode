e9ce565ca6e1f354d0a72087cc8c165c118ee5ac
Annii <anish.1903.kmr@gmail.com>
2021-07-29 15:37:32 +0530

    solved question 15

---
+++ Arrays/Q.15__Best_Time_To_Buy_And_Sell_Stocks.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int amountToBuy = prices[0];
+        int maxProfit = 0;
+        
+        // printf("%d",prices.size());
+        
+        if(prices.size()>1){
+            
+            for(int i=0;i<prices.size(); i++){
+                if(prices[i]<amountToBuy){
+                    amountToBuy = prices[i];
+                }
+                else if(prices[i]-amountToBuy > maxProfit){
+                    maxProfit = prices[i]-amountToBuy;
+                }
+            }
+        }
+        return maxProfit;
+    }
+};
+
