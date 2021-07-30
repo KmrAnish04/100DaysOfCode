d2af4f983087c4e5ad72a181c723a0975ddf3afd
Annii <anish.1903.kmr@gmail.com>
2021-07-30 23:35:38 +0530

    modified

--- Arrays/Q.15__Best_Time_To_Buy_And_Sell_Stocks.cpp
+++ Arrays/Q.15__Best_Time_To_Buy_And_Sell_Stocks.cpp
@@ -3,18 +3,13 @@ public:
     int maxProfit(vector<int>& prices) {
         int amountToBuy = prices[0];
         int maxProfit = 0;
-        
-        // printf("%d",prices.size());
-        
-        if(prices.size()>1){
             
-            for(int i=0;i<prices.size(); i++){
-                if(prices[i]<amountToBuy){
-                    amountToBuy = prices[i];
-                }
-                else if(prices[i]-amountToBuy > maxProfit){
-                    maxProfit = prices[i]-amountToBuy;
-                }
+        for(int i=0;i<prices.size(); i++){
+            if(prices[i]<amountToBuy){
+                amountToBuy = prices[i];
+            }
+            else if(prices[i]-amountToBuy > maxProfit){
+                maxProfit = prices[i]-amountToBuy;
             }
         }
         return maxProfit;
