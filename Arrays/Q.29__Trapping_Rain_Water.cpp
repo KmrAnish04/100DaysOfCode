435810f4ff2f42ad8dccf1d4f5852ae2110acaa5
Annii <anish.1903.kmr@gmail.com>
2021-09-07 14:56:41 +0530

    solved segmentaion falut for the program in q.29

--- Arrays/Q.29__Trapping_Rain_Water.cpp
+++ Arrays/Q.29__Trapping_Rain_Water.cpp
@@ -3,29 +3,20 @@
 
 long long trappingWater(int arr[], int n){
         // code here
-        int maxL[n];
+        int maxL = arr[0]; 
         int maxR[n];
-        int water[n];
         long long ans=0;
         
-        maxL[0]=arr[0];
         maxR[n-1]=arr[n-1];
-        
-        for(int i=1; i<n; i++){
-            maxL[i] = max(maxL[i-1], arr[i]);
-        }
         
         for(int j=n-2; j>=0; j--){
             maxR[j] = max(maxR[j+1], arr[j]);
         }
         
         for(int k=0; k<n; k++){
-            water[k] = min(maxL[k], maxR[k]) - arr[k];
+            maxL = max(maxL, arr[k]); // this maxL variable doing the same thing as maxR for left array.
+            ans += min(maxL, maxR[k]) - arr[k];
         }
         
-        for(int l=0; l<n; l++){
-            ans += water[l];
-        }
         
-        return ans;
     }
