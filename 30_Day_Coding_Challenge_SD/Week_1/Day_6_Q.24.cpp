39562a851c4ce1e70c2f319e4ea4e265073bcae6
Annii <anish.1903.kmr@gmail.com>
2021-09-12 00:38:31 +0530

    solved week1 day6 ques

---
+++ 30_Day_Coding_Challenge_SD/Week_1/Day_6_Q.24.cpp
@@ -0,0 +1,14 @@
+string catAndMouse(int x, int y, int z) {
+    
+    
+    if(abs(z-x)<abs(z-y)){
+        return "Cat A";
+    }
+    else if(abs(z-y)<abs(z-x)){
+        return "Cat B";
+    }
+    else{
+        return "Mouse C";
+    }
+    
+}
