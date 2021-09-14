a224926ece5a3931cfe6bad0a4bc4ac96c1e9465
Annii <anish.1903.kmr@gmail.com>
2021-09-14 15:57:29 +0530

    renamed

---
+++ 30_Day_Coding_Challenge_SD/Week_2/Day_7_Q.27.cpp
@@ -0,0 +1,59 @@
+vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player)
+{
+    map<int, int> mapArr;
+    vector<int> res;
+
+    /* I taken map her but not used hashing, I taken map only for storing "ranked" array elements in sorted order(accessending order) and for not including repeated
+       elements.
+       We can take any container instead of map, but the only thing to take care is while storing elements in that container just make sure that it should be stored in 
+       sorted order(accessending order) and no repeataion should be included of any element. 
+    */
+   
+    for (int i = 0; i < ranked.size(); i++) 
+    {
+        mapArr[ranked[i]]++;
+    }
+
+    int j = 0;
+    int rank = mapArr.size();
+    map<int, int>::iterator i;
+    for (i = mapArr.begin(); i != mapArr.end();)
+    {
+
+        if (j < player.size())
+        {
+
+            if (player[j] < (i->first) && j < player.size())
+            {
+                res.push_back(rank + 1);
+                j++;
+            }
+            else if (player[j] == (i->first) && j < player.size())
+            {
+                res.push_back(rank);
+                j++;
+            }
+            else if (player[j] >= (i->first) && rank == 1 && j < player.size())
+            {
+                res.push_back(1);
+                j++;
+            }
+            else
+            {
+                rank--;
+                i++;
+                continue;
+            }
+        }
+
+        else
+        {
+            break;
+        }
+    }
+
+    return res;
+}
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
