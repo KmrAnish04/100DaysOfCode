0a7c0c62329562ef8e1c5b345d6f34b0ad7946a4
Annii <anish.1903.kmr@gmail.com>
2021-09-24 12:34:15 +0530

    solved day12 q.1

---
+++ 30_Day_Coding_Challenge_SD/Week_2/Day_12_Q.1.cpp
@@ -0,0 +1,20 @@
+/*
+    Solution by Anish.
+*/
+
+int jumpingOnClouds(vector<int> c, int k) {
+    int size=c.size();
+    int Eng=100;
+    int curPos=0;
+    bool reached=false;
+    
+    while(!reached){
+        int nxtJmpIdx=(curPos+k)%size;
+        curPos=nxtJmpIdx;
+        Eng--;
+        if(c[curPos]==1) Eng -= 2;
+        if(curPos==0) reached=true;        
+    }
+    
+    return Eng;
+}
