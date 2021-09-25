e0bb76188a680b96b386e036fc51d3015773c7d8
Annii <anish.1903.kmr@gmail.com>
2021-09-26 00:00:56 +0530

    solved day12 q.3

--- 30_Day_Coding_Challenge_SD/Week_2/Day_12_Q.3.cpp
+++ 30_Day_Coding_Challenge_SD/Week_2/Day_12_Q.3.cpp
@@ -1,23 +1,28 @@
 int squares(int a, int b) {
     float sqtAf=sqrt(a);
     int sqtAi=sqrt(a);
-    int diff= sqtAf-sqtAi;
+    float diff= sqtAf-sqtAi;
     int fstNum;
     int cnt=0;
     
     if(diff==0) cnt++;
+    
     while(diff!=0 && a<=b){
         a++;
+        sqtAf=sqrt(a);
+        sqtAi=sqrt(a);
         diff=sqtAf-sqtAi;
         if(diff==0) cnt++;
     }
+    
     if(a>b) return 0;
     else if(a==b) return 1;
-    fstNum=a;
     
+    fstNum=a;
     if(fstNum<b){
         int sqt = sqrt(fstNum);
-        while((sqt+1)*(sqt+1)<=b){
+        sqt++;
+        while((sqt*sqt)<=b){
             cnt++;
             sqt++;
         }
