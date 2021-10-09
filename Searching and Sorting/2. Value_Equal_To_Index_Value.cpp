879fdfce466bcc1750ebc1d3f2798e48eff77439
Annii <anish.1903.kmr@gmail.com>
2021-10-09 20:50:25 +0530

    started solving searching sorting ques

---
+++ Searching and Sorting/2. Value_Equal_To_Index_Value.cpp
@@ -0,0 +1,12 @@
+class Solution{
+public:
+
+	vector<int> valueEqualToIndex(int arr[], int n) {
+	    // code here
+	    vector<int> ans;
+	    for(int i=0; i<n; i++)
+	        if(i+1==arr[i]) ans.push_back(i+1);
+	    
+	    return ans;
+	}
+};
