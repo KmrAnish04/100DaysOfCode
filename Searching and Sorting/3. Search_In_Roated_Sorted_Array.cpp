f3190d3339418dc28b760959ee23e90f571fe6e8
Annii <anish.1903.kmr@gmail.com>
2021-10-18 23:32:06 +0530

    solved

--- Searching and Sorting/3. Search_In_Roated_Sorted_Array.cpp
+++ Searching and Sorting/3. Search_In_Roated_Sorted_Array.cpp
@@ -1,50 +1,59 @@
 class Solution {
-public:
-    int binarySearch(vector<int> array, int x, int low, int high) {
-  
-        // Repeat until the pointers low and high meet each other
-          while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (array[mid] == x)
-              return mid;
-
-            if (array[mid] < x)
-              low = mid + 1;
-
+    int find_pivot(vector<int>& nums,int left,int right)
+    {
+        if(nums[left] < nums[right])
+            return 0;
+        
+        int mid;
+        //Iterative binary search
+        while(left<right)
+        {
+            mid = (left+right)/2;
+            if(nums[mid]>nums[mid+1])
+                return mid;
+            else if(nums[mid]<nums[0])
+                right = mid;
             else
-              high = mid - 1;
-          }
-
-          return -1;
+                left = mid+1;
+        }
+        return mid;
     }
-
+    
+    int binary_search(vector<int>& nums,int left,int right,int target)
+    {
+        int mid;
+        //Iterative binary search
+        while(left<=right)
+        {
+            mid = (left+right)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                right = mid-1;
+            else
+                left = mid+1;
+        }
+        return -1;
+    }
+public:
     int search(vector<int>& nums, int target) {
-        int index = nums.size()-1;
-        // int i;
-        int ans;
+        int n = nums.size();
+        if(n==0)
+            return -1;
+        else if(n==1)
+            return nums[0]==target?0:-1;
         
-        if(nums.size()==1){
-            if(nums[0]==target) return 0;
-            else return -1;
+        int pivot = find_pivot(nums,0,n-1);
+        if(nums[pivot]==target)
+            return pivot;
+        
+        if(target>=nums[0] && target<=nums[pivot]){
+            return binary_search(nums, 0, pivot, target);
+        }
+        else{
+            return binary_search(nums, pivot+1, nums.size()-1, target);
         }
         
-        for(int i=nums.size()-1; i>=0; i--){
-            if(nums[i-1]>nums[i]){
-                index=i;
-                break;
-            }
-            else index--;
-        }
-        
-        cout<<"index="<<index<<"\n";
-        reverse(nums.begin(), nums.end()-(index-1));
-        reverse(nums.begin()+index, nums.end());
-        reverse(nums.begin(), nums.end());
-        for(int i=0; i<nums.size(); i++) cout<<nums[i]<<" ";
-        
-        ans = binarySearch(nums, target, 0, nums.size()-1);
-        if(ans<0) return -1;
-        return ans+index;
+        return -1;
     }
 };
