9fa704ba30bd1530f1ba61d87fa9a4d2ced08f4b
Annii <anish.1903.kmr@gmail.com>
2021-10-19 23:09:26 +0530

    solved

--- Searching and Sorting/8. Find_Majority_Element.cpp
+++ Searching and Sorting/8. Find_Majority_Element.cpp
@@ -1,1 +1,39 @@
-Moore's Voting Algorithm
+/*
+    Moore's Voting Algorithm
+    Time Complexity: O(N)
+    Space Complexity: O(1)
+
+*/
+
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+    int majorityElement(int a[], int size)
+    {
+        
+        // your code here
+        int cnt1=1;
+        int cnt2=0;
+        int res=0;
+        
+        for(int i=1; i<size; i++){
+            if(a[res]==a[i]) cnt1++;
+            else cnt1--;
+            
+            if(cnt1==0){
+                cnt1=1;
+                res=i;
+            }
+        }
+        
+        for(int i=0; i<size; i++){
+            if(a[res]==a[i]) cnt2++;
+        }
+        
+        if(cnt2 > (size/2)) return a[res];
+        
+        return -1;
+    }
+};
