421a28a4781ee40e97443db1708cca70eb06dc9e
Annii <anish.1903.kmr@gmail.com>
2021-10-28 22:54:45 +0530

    not Solved yet

--- Searching and Sorting/19. Bishu_And_Soldiers.cpp
+++ Searching and Sorting/19. Bishu_And_Soldiers.cpp
@@ -8,22 +8,25 @@ int binarySearch(vector<int> arr, int l, int r, int x)
 {
     int index;
     while (l <= r) {
-        int m = l + (r - l) / 2;
+        int m = (l+r)/ 2;
  
         // Check if x is present at mid
         if (arr[m] == x)
-            return m;
+            return m+1;
  
         // If x greater, ignore left half
         if (arr[m] < x){
             l = m + 1;
+            index=m+1;
         }
  
         // If x is smaller, ignore right half
-        else
+        else{
             r = m - 1;
-
-        index=m;
+            index=m;
+        }
+        // index = m;
+        
     }
  
     // if we reach here, then element was
@@ -42,32 +45,32 @@ vector<int> bishuFight(vector<int> pows, int bishuPower){
     }
 
     int a = binarySearch(pows, 0, pows.size()-1, bishuPower);
-    if (a==0)
-    {
-        if (bishuPower>= pows[0])
-        {
-            result.push_back(a+1);
-            result.push_back(preSum[0]);
-        }
-        else{
-            result.push_back(0);
-            result.push_back(0);
-        }
-    }
-    else{
-        result.push_back(a+1);
-        result.push_back(preSum[a]);
-    }
-    
+    // if (a==0)
+    // {
+    //     if (bishuPower>= pows[0])
+    //     {
+    //         result.push_back(a+1);
+    //         result.push_back(preSum[0]);
+    //     }
+    //     else{
+    //         result.push_back(0);
+    //         result.push_back(0);
+    //     }
+    // }
+    // else{
+    //     result.push_back(a);
+    //     result.push_back(preSum[a]);
+    // }
+    result.push_back(a);
+    result.push_back(preSum[a-1]);
 
-
-    cout<<"binarySearch result="<<a<<endl;
+    // cout<<"binarySearch result="<<a<<endl;
 
     return result;
 }
 
 int main(){
-    cout<<"start"<<endl;
+    // cout<<"start"<<endl;
     int noOfSold;
     vector<int> solPow;
     int noOfRounds;
@@ -82,28 +85,29 @@ int main(){
         // cout<<"input";   
     }
 
-    cout<<noOfSold<<endl; //
-    for (int i = 0; i < noOfSold; i++)
-    {
-        cout<<solPow[i]<<" ";
-    }
+    // cout<<noOfSold<<endl; //
+    // for (int i = 0; i < noOfSold; i++)
+    // {
+    //     cout<<solPow[i]<<" ";
+    // }
 
-    cout<<endl;
+    // cout<<endl;
     
     cin>>noOfRounds;
-    cout<<noOfRounds<<endl; //
+    // cout<<noOfRounds<<endl; //
     while(noOfRounds--){
         int bishuPow;
         cin>>bishuPow;
         // cout<<bishuPow; //
         // cout<<endl; //
+        sort(solPow.begin(), solPow.end());
         vector<int> ans = bishuFight(solPow, bishuPow);
         if (!ans.empty())
         {
             // cout<<"[";
             for (int i = 0; i < ans.size(); i++)
             {
-                cout<<ans[i]<<", ";
+                cout<<ans[i]<<" ";
             }
             // cout<<"]";
         }
