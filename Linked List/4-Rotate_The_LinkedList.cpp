e50922ea3f10989a26d154be1514d3212796d3e8
Annii <anish.1903.kmr@gmail.com>
2022-01-15 17:14:02 +0530

    solved/but can be optimized more

---
+++ Linked List/4-Rotate_The_LinkedList.cpp
@@ -0,0 +1,36 @@
+
+/*
+    Anish's Solution
+    Time: O(n)
+    Space: O(1)
+    --This solution can be optimized more.
+*/
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node* rotate(Node* head, int k)
+    {
+        // Your code here
+        Node *nthNode;
+        Node *kthNode;
+        Node *headNode;
+        
+        int size=0;
+        Node *tmpNode = head;
+        while(tmpNode != NULL){
+            size++;
+            if(size == k) kthNode=tmpNode;
+            nthNode = tmpNode;
+            tmpNode = tmpNode->next;
+        }
+        headNode = kthNode->next;
+        
+        if(k == size) return head;
+        
+        kthNode->next = NULL;
+        nthNode->next = head;
+        
+        return headNode;
+    }
+};
