02db1383910d96adc775c78ea6c897a687a8c2b9
Annii <anish.1903.kmr@gmail.com>
2022-01-15 16:44:32 +0530

    solved

---
+++ Linked List/3-Reverse_LinkedList.cpp
@@ -0,0 +1,33 @@
+
+
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {
+        // code here
+        // return head of reversed list
+        struct Node *pre = NULL;
+        struct Node *curr = head;
+        struct Node *next = NULL;
+        
+        struct Node *tmp = head;
+        int size = 0;
+        while(tmp != NULL){
+            size++;
+            tmp = tmp->next;
+        }
+        
+        for(int i=0; i<size; i++){
+            next = curr->next;
+            curr->next = pre;
+            pre = curr;
+            curr = next;
+        }
+        
+        return pre;
+    }
+    
+};
+    
