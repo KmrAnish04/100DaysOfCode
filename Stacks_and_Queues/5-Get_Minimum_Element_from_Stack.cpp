6779bd4e6e33eacdf10ed05bea356ac8fbeef967
Annii <anish.1903.kmr@gmail.com>
2022-02-01 10:59:51 +0530

    solved

--- Stacks_and_Queues/5-Get_Minimum_Element_from_Stack.cpp
+++ Stacks_and_Queues/5-Get_Minimum_Element_from_Stack.cpp
@@ -73,3 +73,73 @@ public:
         }
     }
 };
+
+
+
+/*
+
+    Algorithm:
+
+        1. Define a variable minEle that stores the current minimum element in the stack. When a minEle is removed fromt he stack, we push “2x – minEle” into the stack instead of x so that previous minimum element can be retrieved using current minEle and its value stored in stack.
+
+        Push(x) : Inserts x at the top of stack.
+
+        If stack is empty, insert x into the stack and make minEle equal to x.
+        If stack is not empty, compare x with minEle. Two cases arise:
+        If x is greater than or equal to minEle, simply insert x.
+        If x is less than minEle, insert (2*x – minEle) into the stack and make minEle equal to x.
+        Pop() : Removes an element from top of stack.
+
+        Remove element from top. Let the removed element be y. Two cases arise:
+        If y is greater than or equal to minEle, the minimum element in the stack is still minEle.
+        If y is less than minEle, the minimum element now becomes (2*minEle – y), update (minEle = 2*minEle – y). Now retrieve previous minimum from current minimum and its value in stack.
+
+
+*/
+
+// Solution by GFG
+class Solution{
+    int minEle;
+    stack<int> s;
+    public:
+       int getMin(){
+           if(s.empty()){
+             return -1;
+           }
+           else{
+             return minEle;
+           }
+       }
+       int pop(){
+           if(s.empty()){
+                return -1;
+            }
+            int t = s.top();
+            s.pop();
+            if(t<minEle)
+            {
+                int k = minEle;
+                minEle = 2*minEle-t;
+                return k;
+            }
+            else{
+                return t;
+            }
+       }
+       void push(int x){
+           if(s.empty())
+           {
+             minEle =x ;
+             s.push(x);
+             return ;
+            }
+            if(x<minEle)
+            {
+                s.push(2*x-minEle);
+                minEle = x;
+            }
+            else{
+                s.push(x);
+            }
+       }
+};
