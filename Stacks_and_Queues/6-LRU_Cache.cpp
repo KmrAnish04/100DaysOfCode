1d4e8b6791be2b2ef16576d8e3196d197b2c8d6d
Annii <anish.1903.kmr@gmail.com>
2022-02-06 00:20:06 +0530

    not solved yet

--- Stacks_and_Queues/6-LRU_Cache.cpp
+++ Stacks_and_Queues/6-LRU_Cache.cpp
@@ -16,7 +16,7 @@ class LRUCache
 
     public:
     unordered_map<int, node*> keyMap;
-    queue<node> que;
+    // queue<node> que;
     node *head=new node(-1, -1);
     node *front=head;
     node *back=head;
@@ -33,50 +33,91 @@ class LRUCache
     int get(int key)
     {
         // your code here
-        return back->value;
+        if (keyMap.find(key) == keyMap.end()) {
+            return -1; //Key is not in the map
+        }
+        
+        node *reqNode=keyMap[key];
+        int popedData=reqNode->value;
+        
+        if(back!=front){
+            // pop this node from its origional position and push into the back of LL
+            // poping from the front
+            if(back!=reqNode){
+                
+                reqNode->pre->next=reqNode->next;
+                reqNode->next->pre=reqNode->pre;
+                
+                // pushing into the front
+                back->next=reqNode;
+                reqNode->next=NULL;
+                reqNode->pre=back;
+                
+                tmp=reqNode;
+                back=reqNode;
+            }
+        }
+        
+        return popedData;
     }
     
     //Function for storing key-value pair.
     void set(int key, int value)
     {
         // your code here   
-        node *newNode=new node(key, value);
-        if(keyMap.size()<cacheSize){
-            if(tmp->key==-1){
-                tmp->next=newNode;
-                newNode->pre=tmp;
-                front = newNode;
-                back = newNode;
-            }
-            else{
-                tmp->next=newNode;
-                newNode->pre=tmp;
-                back = newNode;
-                
-            }
-            tmp=newNode;
-            keyMap[key] = newNode;
+        if(keyMap.find(key)!=keyMap.end()){
+            node *nodeRepl = keyMap[key];
+            nodeRepl->pre->next = nodeRepl->next;
+            nodeRepl->next->pre=nodeRepl->pre;
             
+            back->next=nodeRepl;
+            nodeRepl->next=NULL;
+            nodeRepl->pre=back;
             
+            nodeRepl->value=value;
+            tmp=nodeRepl;
+            back=nodeRepl;
         }
         else{
-            //firstly we will pop from front, then push from back
             
-            // Poping from front
-            node *firstNode=head->next;
-            head->next=firstNode->next;
-            firstNode->next->pre=head;
-            keyMap.erase(firstNode->key);
-            // keyMap[firsNode->key]=NULL;
-            free(firstNode);
-            front= head->next;
-            
-            // Pushing from back
-            back->next=newNode;
-            newNode->pre=back;
-            
-            
-            
+            node *newNode=new node(key, value);
+            if(keyMap.size()<cacheSize){
+                if(tmp->key==-1){
+                    tmp->next=newNode;
+                    newNode->pre=tmp;
+                    front = newNode;
+                    back = newNode;
+                }
+                else{
+                    tmp->next=newNode;
+                    newNode->pre=tmp;
+                    back = newNode;
+                    
+                }
+                tmp=newNode;
+                keyMap[key] = newNode;
+                
+                
+            }
+            else{
+                //firstly we will pop from front, then push from back
+                
+                // Poping from front
+                node *firstNode=head->next;
+                head->next=firstNode->next;
+                firstNode->next->pre=head;
+                keyMap.erase(firstNode->key);
+                // keyMap[firsNode->key]=NULL;
+                free(firstNode);
+                front= head->next;
+                
+                // Pushing from back
+                back->next=newNode;
+                newNode->pre=back;
+                
+                
+                
+            }
         }
     }
 };
