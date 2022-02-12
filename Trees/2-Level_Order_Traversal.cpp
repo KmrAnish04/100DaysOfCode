96a476b511bbcc8aeb68ab28397a6a9815c46005
Annii <anish.1903.kmr@gmail.com>
2022-02-12 13:11:30 +0530

    solved

--- Trees/2-Level_Order_Traversal.cpp
+++ Trees/2-Level_Order_Traversal.cpp
@@ -1,3 +1,78 @@
+/*
+    Problem: Level Order Traversal
+    Problem LinkL:
+            GFG: https://practice.geeksforgeeks.org/problems/level-order-traversal/1
+            LeetCode: https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+
+    *** Complexity Analysis: ***
+        Time: O(N)
+        Space: O(N)
+
+*/
+
+
+// *** GFG Solution ***
+
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* node)
+    {
+      //Your code here
+      vector<int> ans;
+      queue<Node*> nodeQue;
+      
+      if(node==NULL) return ans;
+      
+      nodeQue.push(node);
+      while(!nodeQue.empty()){
+          
+          Node *front = nodeQue.front();
+          ans.push_back(front->data);
+          if(front->left != NULL){
+              nodeQue.push(front->left);
+          }
+          if(front->right != NULL){
+              nodeQue.push(front->right);
+          }
+          
+          nodeQue.pop();
+      }
+    }
+};
+
+
+
+
+// *** LeetCode Solution ***
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
@@ -8,56 +83,28 @@ public:
             return ans;
         
         nodeQue.push(root);
-        int currLevel=0;
         
         while(!nodeQue.empty()){
             
             vector<int> level;
-            TreeNode *queFront = nodeQue.front();
+            int sizeOfQue = nodeQue.size();
             
-            if(queFront==root){
-                ans.push_back({queFront->val});
-                
+            for(int i=0; i<sizeOfQue; i++){
+                TreeNode *queFront = nodeQue.front();
+                level.push_back(queFront->val);
                 if(queFront->left!=NULL){
                     nodeQue.push(queFront->left);
-                    level.push_back(queFront->left->val);
                 }
                 if(queFront->right!=NULL){
                     nodeQue.push(queFront->right);
-                    level.push_back(queFront->right->val);
                 }
                 nodeQue.pop();
-                ans.push_back(level);
-                currLevel++;
                 
             }
-            else{
-                for(int i=0; i<pow(2,currLevel); i++){
-                    TreeNode *front=nodeQue.front();
-                    // if(front->left==NULL) cout<<"left="<<front->left<<endl;
-                    if(front->left != 0){
-                        nodeQue.push(nodeQue.front()->left);
-                        level.push_back(nodeQue.front()->left->val);
-                    }
-                    if(nodeQue.front()->right!=0){
-                        nodeQue.push(nodeQue.front()->right);
-                        level.push_back(nodeQue.front()->right->val);
-                    }
-
-                    nodeQue.pop();
-                }
-            
-                // ans.push_back(level);
-                // nodeQue.pop();
-                // currLevel++;
-                
-                ans.push_back(level);
-                currLevel++;
-            }
-            // ans.push_back(level);
-            // currLevel++;
+            ans.push_back(level);
         }
         
         return ans;
     }
 };
+
