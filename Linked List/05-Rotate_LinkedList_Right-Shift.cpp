84f354464363c5a1c2e72d0b1df5971fb908b659
Annii <anish.1903.kmr@gmail.com>
2022-09-10 00:05:15 +0530

    Solved

--- Linked List/05-Rotate_LinkedList_Right-Shift.cpp
+++ Linked List/05-Rotate_LinkedList_Right-Shift.cpp
@@ -1,9 +1,23 @@
+// *************************** LinkedList ***************************
+
 /*
-    Anish's Solution
-    Question Link: https://leetcode.com/problems/rotate-list/
-    Time: O(n)
-    Space: O(1)
-    --This solution can be optimized more.
+    Problem Description: 
+    Question Link: 
+        GFG: 
+        Leetcode: https://leetcode.com/problems/rotate-list/
+        pepcoding: 
+        nados: 
+
+    Complexity Analysis:
+        Iterative Approach
+            Time Complexity: O(N)
+            Space Complexity: O(1)
+
+
+
+    Algorithm:
+        There is no trick for this question, just find the new head, new tail (make new tail's next point towards NULL) and reconnect the new middle node with previous head node. 
+        This is lame approaches which is best for this question.
 */
 
 class Solution {
@@ -45,4 +59,47 @@ public:
         return headNode;
         
     }
+
+
+    // Almost same approach as above, but with a changed style
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+        int sizei=0;
+        ListNode *tempNode = head;
+        while(tempNode!=NULL){
+            sizei++;
+            tempNode = tempNode->next;
+        }
+        
+        if(k<=0 || sizei<=1) return head;
+        
+        k = k%sizei;
+        int m = sizei-k; 
+        if(m == sizei) return head;
+        
+        ListNode *newTail;
+        ListNode *newHead;
+        ListNode *newMid;
+        ListNode *tempNodes=head;
+        int size=1;
+        
+        while(tempNodes){
+            if(size==m){
+                newTail = tempNodes;
+                newHead = tempNodes->next;
+            }
+            newMid = tempNodes;
+            tempNodes = tempNodes->next;
+            size++;
+        }
+        
+        newMid->next = head;
+        newTail->next = NULL;
+        
+        
+        return newHead; 
+        
+    }
+
+
 };
